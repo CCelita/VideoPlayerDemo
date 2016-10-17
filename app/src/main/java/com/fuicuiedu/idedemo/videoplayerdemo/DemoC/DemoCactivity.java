fbe9c0fc7a99e9d32c5dc678cff724edc2643a3f
wxcican <wxcican@qq.com>
2016-10-17 16:01:59 +0800

    Demo框架搭建，加入Vitamio

---
+++ app/src/main/java/com/fuicuiedu/idedemo/videoplayerdemo/DemoC/DemoCactivity.java
@@ -0,0 +1,15 @@
+package com.fuicuiedu.idedemo.videoplayerdemo.DemoC;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fuicuiedu.idedemo.videoplayerdemo.R;
+
+public class DemoCactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_c);
+    }
+}
